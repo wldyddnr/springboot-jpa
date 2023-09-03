4f5741911247e6aa48bed60496cec396385c64b3
yonguk <wldyddnr@gmail.com>
2023-09-03 23:22:05 +0900

    스프링 데이터 jpa

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -29,6 +29,7 @@ public class MemberService {
 
     public void validateDuplicateMember(Member member) {
 
+
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
@@ -45,13 +46,13 @@ public class MemberService {
 
     public Member findOne(Long id) {
 
-        return memberRepository.findOne(id);
+        return memberRepository.findById(id).get();
     }
 
 
     @Transactional
     public void update(Long id, String name) {
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 }
